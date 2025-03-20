7b89534239e6cbd0c562c429e0090f2816c8a944
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-20 11:52:21 +0530

    sn

--- Functions/code.js
+++ Functions/code.js
@@ -11,9 +11,28 @@
 // add(10,31)//NaN  unable to perform +,-,*, concatenation
 // add(123)//NaN   unable to perform +,-,*, concatenation
 
-function add(a,b,c=10){
-    console.log(a+b+c);
+// function add(a,b,c=10){
+//     console.log(a+b+c);
+// }
+// add(1213,143,14)//1370
+// add(10,31)//51
+// add(123)//NaN   unable to perform +,-,*, concatenation
+
+function add(a,b){
+    console.log(a+b)
+    console.log(a);
+    console.log(b)
 }
-add(1213,143,14)//1370
-add(10,31)//51
-add(123)//NaN   unable to perform +,-,*, concatenation
+add(10,10)
+/*
+20
+10
+10
+*/
+add(20,50)
+/*
+70
+20
+50
+*/
+
