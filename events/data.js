89ef2324bba30845c001bd06457998a091a40d1a
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-21 12:29:43 +0530

    sn

---
+++ events/data.js
@@ -0,0 +1,9 @@
+// // function display() {
+// //     alert("Event triggered!");
+// // }
+// document.getElementsByid('abc').innerHTML='good morning'
+// document.getElementsByTagName('button')[0].innerHTML='good morning'
+// document.getElementsByTagName('button')[1].innerHTML='good morning'
+button.addEventListener('mouseover'), () => {
+      button.style.backgroundColor = 'blue'
+}
