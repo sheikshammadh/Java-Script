6abd306c22ece28faef8247c98e7934c2f890b82
sheikshammadh <sheikshammadh18@gmail.com>
2025-03-24 12:44:31 +0530

    sn

---
+++ fat_arrow function/two.js
@@ -0,0 +1,56 @@
+// //normal func:
+// function addition(a,b){
+//     return a+b+1
+// }
+// let r1=addition(10,20)
+// console.log(r1);//31
+// let r2=addition(1,2)
+// console.log(r2);//4
+
+
+// //FA func:
+// let add=(a,b)=>{
+//     return a+b+1
+// }
+// let r3=add(10,20)
+// console.log(r1);//31
+// let r4=add(1,2)
+// console.log(r2);//4
+
+
+// //
+// let ad=(a,b)=>a+b+1;
+
+// let r5=ad(10,20)
+// console.log(r1);//31
+// let r6=ad(1,2)
+// console.log(r2);//4
+// ============================================================================================================================================
+// function wish(rahul){
+//     console.log('good morning');
+
+// }
+// wish()//good morning
+
+// let wish=name=>'hello'+name;
+// console.log(wish('shyam'));//helloshyam
+// ======================================================================================================================================
+
+//to print gm for every 4 seconds
+// setTimeout()
+// setInterval(1,2)
+// setInterval(() => {
+//     console.log('good morning');
+// }, 4000);
+// ============================================================================================================================================
+//to print gm for every second.
+// setInterval(() => {
+//     console.log('gm');
+// },1000)
+// ==================================================================================================================================
+
+setTimeout(1,2)
+// setInterval(1,2)
+settimeout(() => {
+    console.log('good morning');
+}, 4000);
