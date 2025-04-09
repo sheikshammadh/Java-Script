d110ee6d04dd72e765312039529f16002aac497f
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-09 14:44:57 +0530

    sn

--- conditional stae.js/for.whileloop.js
+++ conditional stae.js/for.whileloop.js
@@ -1,4 +1,4 @@
-//for
+// for
 // while
 // do while
 
@@ -6,26 +6,26 @@
 // conditional statement.(value)
 // increment or decrement.(value)
 
-// // to print 1 to 100.
-// for (i=1;i<=100;i=i+1){
-//     console.log(i);
-// }
-// // to print 100 to 50.
-// for (let i=100;i>=51;i=i-1){
-//     console.log(i)
-// }
-// // to print first 10 even numbers.
-// for (let i=2;i<=20;i=i+2){
-//     console.log(i)
-// }
-// // by using while iterating from 1 to 100.
-// i=0
-// while(i<=100){
-//     console.log(i)
-//     i=i+1
-// }
-// i=+1
-// while(i<=100){
-//     console.log(i)
-//     i++
-// }
+// to print 1 to 100.
+for (i=1;i<=100;i=i+1){
+    console.log(i);
+}
+// to print 100 to 50.
+for (let i=100;i>=51;i=i-1){
+    console.log(i)
+}
+// to print first 10 even numbers.
+for (let i=2;i<=20;i=i+2){
+    console.log(i)
+}
+// by using while iterating from 1 to 100.
+i=0
+while(i<=100){
+    console.log(i)
+    i=i+1
+}
+i=+1
+while(i<=100){
+    console.log(i)
+    i++
+}
