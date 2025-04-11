c814b26cc58b3ac77c0f6ccfd266fea9264515c7
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-11 13:49:12 +0530

    sn

--- conditional stae.js/condi.iters.js
+++ conditional stae.js/condi.iters.js
@@ -6,11 +6,19 @@
 //     console.log("not good");
 // }
 
-let input=requre('prompt-sync')();
-let num=parseInt(input('enter number:'));
-if(num%2==0){
-    console.log("even number");
-}
-else{
-    console.log("odd");
+// let input=requre('prompt-sync')();
+// let num=parseInt(input('enter number:'));
+// if(num%2==0){
+//     console.log("even number");
+// }
+// else{
+//     console.log("odd");
+// }
+let i=10;
+while(i<=20){
+    if(i>=15){
+     break    
+    }
+    console.log(i);
+    i=i-1
 }
