5c0a89b42ddb2c041194c37c1d839985056a5813
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-11 17:47:24 +0530

    sn

---
+++ inbuilt_methods/ine.js
@@ -0,0 +1,9 @@
+
+
+console.log(Math.random()*10);//
+console.log(Math.floor(5.5));//decrease to nearest value
+console.log(Math.ceil(5.5));//increase to nearest value
+console.log(Math.round(5.5));//.5 above values are increased and below 0.5 decreses value.
+// pow:rise base to the power of a value
+
+
