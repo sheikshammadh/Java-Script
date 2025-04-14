3baa6a18206dd2148142db900ec3912f47cce597
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-14 18:10:00 +0530

    sn

---
+++ ES6/arrayfunc/sync_and_assync/code.js
@@ -0,0 +1,54 @@
+// console.log("request1");
+// console.log("request2");
+
+
+// setTimeout(()=>{console.log("request1");},5000)//executing after 5sec
+// setTimeout(()=>{console.log("request2");},3000)//executing after 3sec
+// setTimeout(()=>{console.log("request3");},1000)//executing after 1sec
+// setTimeout(()=>{console.log("request4");},2000)//executing after 2sec
+
+
+// let calc=(a,b,op)=>{
+// if(op==="sum"){
+//     return a+b;
+// }
+// if(op==="multi"){
+//     return a*b
+// }
+// }
+// let r1=calc(10,20,"sum")
+// let r2=calc(10,20,"multi")
+// console.log(r1);//30
+// console.log(r2);//200
+
+
+
+// let calc=(a,b,op)=>{
+//     if(op==="sum"){
+//         return a+b;
+//     }
+//     if(op==="multi"){
+//         return a*b
+//     }
+//     if(op==="callback"){
+//         return callback(a,b)
+//     }
+//     }
+//     let r1=calc(10,20,"sum")
+//     let r2=calc(10,20,"multi")
+//     console.log(r1);//30
+//     console.log(r2);//200
+    
+// ==============================================================================================================
+
+let emp=[
+    {eid:100,}
+    {}
+]
+let create_new_emp=()=>{
+    
+}
+let disp_emp=()=>{
+
+}
+create_new_emp({})
