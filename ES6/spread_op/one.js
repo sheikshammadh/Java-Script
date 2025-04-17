5fdd33c0e4b3a3f64694a95138853e60120a6ff4
sheikshammadh <sheikshammadh18@gmail.com>
2025-04-17 16:07:55 +0530

    sn

--- ES6/spread_op/one.js
+++ ES6/spread_op/one.js
@@ -31,3 +31,11 @@
 // console.log(emp_details);
 //object doesnt allowed duplicate keys/properties.
 
+function add(a,...b){
+    console.log("a value",a,"and b value",b);
+}
+add(10,20)
+add(10,20,30)
+add(10,20,30,40)
+add(10,20,30,40,50)
+add(10,20,30,40,50,60)
